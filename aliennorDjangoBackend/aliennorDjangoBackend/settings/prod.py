00549b91c456133ebc20f21a321d001cc1b00944
Cong Cuong PHAM <ccuong.ph@gmail.com>
2018-03-05 09:24:26 +0700

    modify prod.py

--- aliennorDjangoBackend/aliennorDjangoBackend/settings/prod.py
+++ aliennorDjangoBackend/aliennorDjangoBackend/settings/prod.py
@@ -18,12 +18,15 @@ DATABASES = {
 }
 
 if not DEBUG:
-   AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-   AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-   AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-   STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-   S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-   STATIC_URL = S3_URL
+  AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+  AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+  STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+  S3_UR = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+  STATIC_URL = S3_URL
+  AWS_LOCATION = 'static'
+
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
