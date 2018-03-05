f2d5aa90dd4cea02d42fba18a0cebfd5aaab2430
Cong Cuong PHAM <ccuong.ph@gmail.com>
2018-03-05 09:33:32 +0700

    modify prod.py

--- aliennorDjangoBackend/aliennorDjangoBackend/settings/prod.py
+++ aliennorDjangoBackend/aliennorDjangoBackend/settings/prod.py
@@ -22,11 +22,14 @@ if not DEBUG:
   AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
   AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
   AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-  STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+  STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+  # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
   AWS_LOCATION = 'static'
   S3_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
   STATIC_URL = S3_URL
   AWS_LOCATION = 'static'
+  MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+  DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Parse database configuration from $DATABASE_URL
